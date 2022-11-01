9b097d8cd9241f6e4987e22d3fb2cdf950c08417
yobelagusto <yobel.agusto@gmail.com>
2022-11-01 10:15:20 +0700

    Tugas4

--- app/src/main/java/com/example/pertemuan3/MainActivity.java
+++ app/src/main/java/com/example/pertemuan3/MainActivity.java
@@ -1,20 +1,79 @@
 package com.example.pertemuan3;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 import android.os.Bundle;
+import android.view.MenuItem;
+import com.google.android.material.navigation.NavigationView;
+import android.content.Intent;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        //memanggil drawer_layout dari activity_main.xml
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView =
+                findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        //membuat hamburger icon pada toolbar dan animasinya
+        ActionBarDrawerToggle toggle = new
+                ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        //membuat default navigation menu select
+        if (savedInstanceState == null) {
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new ProfileFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_profile);
+        }
     }
-    public void pasific(View view){
-        Intent a = new Intent(MainActivity.this,pasific.class);
-        startActivity(a);
+
+    //drawer menu fragment handler
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_profile:
+
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ProfileFragment()).commit();
+                break;
+            case R.id.nav_produk:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ProdukFragment()).commit();
+                break;
+            case R.id.nav_chat:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ChatFragment()).commit();
+                break;
+
+        }
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    //on back press behavior
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
     }
 }
